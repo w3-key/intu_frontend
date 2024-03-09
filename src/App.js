9ba6657c68b6000cda8fed5eece70fac6c305afe
Steven Matthiesen <steven.matthiesen@gmail.com>
2024-03-08 17:03:18 -0800

    update to app.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import "./App.css";
-import { Box, Text, Button, AbsoluteCenter } from "@chakra-ui/react";
+import { Spinner, Box, Text, AbsoluteCenter, FormControl, Input, Stack, Button, Heading, VStack, Center } from "@chakra-ui/react";
 import {
   vaultCreation,
   automateRegistration,
@@ -9,13 +9,11 @@ import {
   completeVault,
   getSingleVaultDetails,
   getVault,
-  signTx,
   combineSignedTx,
   submitTransaction,
   _getFilteredUserInitializedLogs,
   getDiscoId,
-  getAllVaultsDetails,
-  getRegistrationStatus,
+  getVaults,
 } from "@intuweb3/exp-web";
 import { ethers } from "ethers";
 import { useState, useEffect } from "react";
@@ -23,56 +21,96 @@ import { SignedIn, SignedOut, SignInButton, SignOutButton } from "@clerk/clerk-r
 import { useAuth } from "@clerk/clerk-react";
 import abi from "./721.json";
 
-const provider = new ethers.providers.JsonRpcProvider("https://testnet.skalenodes.com/v1/giant-half-dual-testnet");
+const provider = new ethers.providers.JsonRpcProvider("https://testnet.skalenodes.com/v1/juicy-low-small-testnet");
+//const provider = new ethers.providers.JsonRpcProvider("https://endpoints.omniatech.io/v1/eth/sepolia/public");
 
 function App() {
   const [currentVault, setCurrentVault] = useState("");
   const { userId } = useAuth();
   const [intuWallet, setIntuWallet] = useState();
   const [intuSigner, setIntuSigner] = useState();
-  const [intuPublic, setIntuPublic] = useState("");
+  const [intuPublic, setIntuPublic] = useState();
+  const [connected, setConnected] = useState();
+  const [gatheringVaults, setGatheringVaults] = useState(false);
+  const [creatingVault, setCreatingVault] = useState(false);
+
+  //useEffect(() => {
+  //  getIntuId().then((res) => {
+  //    console.log(res);
+  //    console.log(currentVault);
+  //    if (currentVault === "" || currentVault === undefined) {
+  //      console.log("createintuvault");
+  //      createIntuVault();
+  //    }
+  //  });
+  //}, [userId, intuWallet, currentVault]);
 
   useEffect(() => {
-    getIntuId();
-    console.log("CurrentVault : " + currentVault);
-    if (currentVault === "") {
-      createIntuVault();
+    async function fetchData() {
+      if (userId && intuWallet === undefined) {
+        const discoId = await getDiscoId(ethers.utils.sha512(ethers.utils.toUtf8Bytes(userId)));
+        const wallet = new ethers.Wallet("0x" + discoId.key);
+        const walletaddress = await wallet.getAddress();
+        setIntuPublic(walletaddress);
+        let signer = wallet.connect(provider);
+        setIntuWallet(wallet);
+        console.log(wallet);
+        setIntuSigner(signer);
+        if (intuPublic) {
+          console.log("CurrentWallet : " + intuPublic);
+        }
+        if (currentVault === "" || currentVault === undefined || Object.keys(currentVault).length === 0) {
+          setGatheringVaults(true);
+          await getVaults(walletaddress, provider).then(async (res) => {
+            if (res && res.length > 0) {
+              setCurrentVault(res[res.length - 1].vaultAddress);
+              console.log(res[res.length - 1]);
+              console.log("CurrentVault : " + res[res.length - 1].vaultAddress);
+              setGatheringVaults(false);
+
+              return res[res.length - 1];
+            } else {
+              console.log("createvault");
+            }
+          });
+          setGatheringVaults(false);
+        }
+      }
     }
+    fetchData();
+    createIntuVault();
   }, [userId, intuWallet, currentVault]);
 
-  const getIntuId = async () => {
-    if (userId && intuWallet === undefined) {
-      const discoId = await getDiscoId(ethers.utils.sha512(ethers.utils.toUtf8Bytes(userId)));
-      const wallet = new ethers.Wallet("0x" + discoId.key);
-      const walletaddress = await wallet.getAddress();
-      setIntuPublic(walletaddress);
-      let signer: ethers.Signer = wallet.connect(provider);
-      setIntuWallet(wallet);
-      setIntuSigner(signer);
-      if (currentVault === "") {
-        _getFilteredUserInitializedLogs(walletaddress, provider).then(async (res) => {
-          if (res && res.length > 0) {
-            setCurrentVault(res[res.length - 1]);
-            console.log("Vault Details below \\/");
-            console.log(await getSingleVaultDetails(res[res.length - 1], provider));
-          }
-        });
-      }
-    } else if (intuWallet && currentVault === "") {
-      _getFilteredUserInitializedLogs(intuPublic, provider).then(async (res) => {
-        console.log(res);
-        if (res && res.length > 0) {
-          setCurrentVault(res[res.length - 1]);
-          console.log(await getSingleVaultDetails(res[res.length - 1], provider));
-        }
+  const connectWallet = async () => {
+    if (!connected) {
+      const provider = new ethers.providers.Web3Provider(window.Ethereum);
+      provider.send("eth_requestAccounts", []).then(async () => {
+        const signer = await provider.getSigner();
+        const _walletAddress = await signer.getAddress();
+        setConnected(true);
+        setIntuPublic(_walletAddress);
       });
     }
   };
 
+  const disconnectWallet = async () => {
+    //window.ethereum.selectedAddress = null;
+    setConnected(false);
+    setIntuPublic("");
+    setIntuWallet("");
+    setIntuSigner("");
+  };
+
   const createIntuVault = async () => {
-    if (intuWallet) {
+    //console.log("intuPublic : " + intuPublic);
+    if (intuPublic && !creatingVault) {
+      setCreatingVault(true);
       let walletAddress = await intuWallet.getAddress();
-      let vaults = await getAllVaultsDetails(walletAddress, provider);
+      let vaults = await getVaults(walletAddress, provider);
+      if (vaults.lenght > 0 && (currentVault === "" || currentVault === undefined || Object.keys(currentVault).length === 0)) {
+        console.log(vaults);
+        setCurrentVault(vaults[0].masterPublicAddress);
+      }
       if (vaults && vaults.length === 0) {
         const d1public = process.env.REACT_APP_NODE_SIGNER_PUBLIC_1;
         const d2public = process.env.REACT_APP_NODE_SIGNER_PUBLIC_2;
@@ -80,32 +118,28 @@ function App() {
         if (intuSigner && intuPublic) {
           console.log("getskale");
           await fetch(`http://18.246.208.46:8888/claim/${intuPublic}`)
-            .then((response) => response.text())
+            //.then((response) => console.log(response.text()))
             .catch((error) => console.log("error", error));
-          await sleep(10000);
           console.log("startvaultcreation");
           const createVaultTransaction = await vaultCreation(proposedAddresses, "NewTestVault", 66, 66, 66, intuSigner);
           const createVaultResult = await createVaultTransaction.wait();
           const vaultAddress = createVaultResult.events[0].address;
+          console.log("createdVault at : " + vaultAddress);
           preRegistration(vaultAddress, intuSigner).then(async () => {
             console.log("start auto reg");
-            await sleep(10000);
+            await sleep(5000);
             automateRegistration(vaultAddress, intuPublic, intuSigner).then(async (result) => {
               console.log("automateregistrationdone");
-              let registerallsteps = (await registerAllSteps(vaultAddress, intuSigner)).wait();
-              console.log(await registerallsteps);
-
+              await registerAllSteps(vaultAddress, intuSigner);
               let vault = await getVault(vaultAddress, provider);
-
               for (let i = 0; i < (await vault.users.length); i++) {
                 let isRegistered = await getUserRegistrationAllInfos(vaultAddress, intuPublic, provider);
                 console.log("registereduserinfo " + i);
                 console.log(isRegistered.registered);
               }
-              await sleep(3000);
+              await sleep(2500);
               await completeVault(vaultAddress, intuSigner).then((res) => {
                 console.log("ALLDONE!!!");
-                // then refresh page
                 _getFilteredUserInitializedLogs(intuPublic, provider).then(async (res) => {
                   console.log(res);
                   if (res && res.length > 0) {
@@ -121,30 +155,48 @@ function App() {
     }
   };
 
-  const sleep = (delay: number) => new Promise((resolve) => setTimeout(resolve, delay));
-
-  // Function to show the spinner
-  //function showSpinner() {
-  //  document.getElementById("spinner").style.display = "block";
-  //}
-  // Function to hide the spinner
-  //function hideSpinner() {
-  //  document.getElementById("spinner").style.display = "none";
-  //}
-
+  const sleep = (delay) => new Promise((resolve) => setTimeout(resolve, delay));
   let submitTx = async (myVaultAddress) => {
-    //const erc721Interface = new ethers.utils.Interface(["function safeTransferFrom(address _from, address _to, uint256 _tokenId)"]);
+    console.log(intuPublic);
     let contractInterface = new ethers.utils.Interface(abi);
-    let d = contractInterface.encodeFunctionData("mint", [intuPublic, "1"]);
-    let chainId = "974399131";
+    let d = contractInterface.encodeFunctionData("mint", [intuPublic, "5"]);
+    console.log(d);
+    //let contract = new ethers.Contract("0x4c7cDa9DcE917C8490577f31d482FAcE62D2D630", abi, intuSigner);
+
+    // get gas price from ethers
+
+    //let chainId = "1351057110";
+    //let chainId = "974399131";
+    let chainId = "11155111";
     let value = "0";
-    let to = "0x3665589D97E04A139f1D95b8c852e7c6F67DFb78"; //this is the smart contract address where we will mint our NFT from
-    let gasPrice = "5000000000000";
-    let gas = "25000000000";
+    let to = "0x374620b9cfc556b6ef09c537ae20007d81ce7832";
+    let gasPrice = "500000000";
+    let gas = "250000";
     let nonce = 0;
     let data = d;
-    await submitTransaction(to, value, chainId, String(nonce), data, gasPrice, gas, myVaultAddress, intuSigner);
+
+    //thought, launch the smart contract on sepolia and try interacting with it there
+    console.log(myVaultAddress);
+    try {
+      await submitTransaction(to, value, chainId, String(nonce), data, gasPrice, gas, myVaultAddress, intuSigner);
+    } catch (e) {
+      console.error(e);
+    }
   };
+
+  //let submitTx = async (myVaultAddress) => {
+  //  //const erc721Interface = new ethers.utils.Interface(["function safeTransferFrom(address _from, address _to, uint256 _tokenId)"]);
+  //  let contractInterface = new ethers.utils.Interface(abi);
+  //  let d = contractInterface.encodeFunctionData("mint", [intuPublic, "1"]);
+  //  let chainId = "11155111";
+  //  let value = "0";
+  //  let to = "0x374620b9cfc556b6ef09c537ae20007d81ce7832"; //this is the smart contract address where we will mint our NFT from
+  //  let gasPrice = "50000000000";
+  //  let gas = "250000";
+  //  let nonce = 0;
+  //  let data = d;
+  //  await submitTransaction(to, value, chainId, String(nonce), data, gasPrice, gas, myVaultAddress, intuSigner);
+  //};
 
   // sign transaction not needed in our example because the nodesigners do it for us.
   //let signTransaction = async (myVaultAddress) => {
@@ -153,13 +205,16 @@ function App() {
   //};
 
   let combineTx = async (myVaultAddress) => {
-    let txId = 2;
+    let txId = 1;
     let hash = await combineSignedTx(myVaultAddress, txId, intuSigner);
-    provider
-      .sendTransaction(hash.combinedTxHash.finalSignedTransaction)
+    console.log("hash to send:");
+    console.log(hash);
+    let p = new ethers.providers.JsonRpcProvider("https://sepolia.infura.io/v3/a6122371120a4819ae02bac868b9d07a");
+
+    p.sendTransaction(hash.combinedTxHash.finalSignedTransaction)
       .then((txResponse) => {
         console.log(txResponse);
-        console.log("https://giant-half-dual-testnet.explorer.testnet.skalenodes.com/tx/" + txResponse.hash);
+        console.log("https://juicy-low-small-testnet.explorer.testnet.skalenodes.com/tx/" + txResponse.hash);
       })
       .catch((error) => {
         console.error("Failed to send transaction:", error);
@@ -171,12 +226,12 @@ function App() {
       <Box h="calc(100vh)" w="100%" bgGradient="linear(to-l, #7928CA, #FF0080)">
         <SignedIn>
           <SignOutButton>
-            <Button>Disconnect...</Button>
+            <Button onClick={() => disconnectWallet()}>Disconnect...</Button>
           </SignOutButton>
         </SignedIn>
         <AbsoluteCenter>
           <SignedOut>
-            <Text fontSize="xl">Welcome to the Gem Game!</Text>
+            <Text fontSize="xl">Welcome to the Gem Game!!</Text>
             <Text fontSize="xl">
               The only game that offers NFT gems <br />
               without needing a wallet!
@@ -186,31 +241,75 @@ function App() {
                 Sign In
               </Button>
             </SignInButton>
+
+            <Text>OR</Text>
+            <Button colorScheme="purple" size="lg" onClick={() => connectWallet()}>
+              Connect Wallet
+            </Button>
           </SignedOut>
 
           <SignedIn>
-            {currentVault === "" ? (
+            {currentVault === "" && !gatheringVaults ? (
               <>
-                <div>
-                  Setting up your account! <br />
-                  In the meantime, why not fill out your profile!
-                </div>
-                <br />
-                <br />
-                <div>SOME EXAMPLE FORM HERE</div>
-                <div>name ..... </div>
-                <div>email .....</div>
-                <div>handle .....</div>
-                <br />
-                <br />
-                <Button>SAVE...</Button> {/*This would be saving the above data to your local database*/}
+                <Center bg="white">
+                  <Stack spacing={4}>
+                    <Stack align="center">
+                      <Heading fontSize="1xl">
+                        {" "}
+                        Setting up your account! <br />
+                        In the meantime, why not fill out your profile!
+                      </Heading>
+                    </Stack>
+                    <VStack as="form" spacing={8} w={{ base: "sm", sm: "lg" }} p={{ base: 5, sm: 6 }}>
+                      <VStack spacing={5} w="100%">
+                        <FormControl id="name">
+                          <Input type="name" placeholder="Your Name" rounded="md" borderBottomLeftRadius="0" borderBottomRightRadius="0" />
+                        </FormControl>
+                        <FormControl id="handle">
+                          <Input type="handle" placeholder="Game Handle" rounded="md" borderBottomLeftRadius="0" borderBottomRightRadius="0" />
+                        </FormControl>
+                        <FormControl id="email">
+                          <Input type="email" placeholder="Email Address" rounded="md" borderBottomLeftRadius="0" borderBottomRightRadius="0" />
+                        </FormControl>
+                        <FormControl id="password" position="relative" bottom="1px">
+                          <Input type="password" placeholder="Password" rounded="md" borderTopLeftRadius="0" borderTopRightRadius="0" />
+                        </FormControl>
+                      </VStack>
+                      <VStack w="100%">
+                        <Button
+                          //leftIcon={<BiLockAlt />}
+                          bg="green.300"
+                          color="white"
+                          _hover={{
+                            bg: "green.500",
+                          }}
+                          rounded="md"
+                          w="100%"
+                        >
+                          Save
+                        </Button>
+                      </VStack>
+                    </VStack>
+                  </Stack>
+                </Center>
               </>
             ) : (
               <>
-                <div>Your web3 account is all set!</div>
-                <div>Time to claim your Gem so you can play the game!</div>
-                <Button onClick={() => submitTx(currentVault)}>Claim My Gem!</Button>
-                <Button onClick={() => combineTx(currentVault)}>Confirm</Button>
+                {!gatheringVaults ? (
+                  <>
+                    <Text>Your web3 account is all set!</Text>
+                    <Text>Time to claim your Gem so you can play the game!</Text>
+                    <Button onClick={() => submitTx(currentVault)}>Claim My Gem!</Button>
+                    <Button onClick={() => combineTx(currentVault)}>Confirm</Button>
+                  </>
+                ) : (
+                  <>
+                    <Text>Gathering your Vaults</Text>
+                    <br />
+                    <br />
+                    <Spinner />
+                  </>
+                )}
               </>
             )}
           </SignedIn>
